6ab91ce3e9da5cbaca3c5e562825c696ba0256a8
Toby <126118957+TxbiG@users.noreply.github.com>
2026-01-17 16:26:52 +0000

    Update XR.cs

--- Moss/XR.cs
+++ Moss/XR.cs
@@ -2,21 +2,6 @@ using System;
 using System.Runtime.InteropServices;
 
 
-using MossXR_Context      = IntPtr;
-using MossXR_Session      = IntPtr;
-using MossXR_Swapchain    = IntPtr;
-using MossXR_Space        = IntPtr;
-using MossXR_Action       = IntPtr;
-using MossXR_ActionSet    = IntPtr;
-using MossXR_Layer        = IntPtr;
-using MossXR_Anchor       = IntPtr;
-using MossXR_Origin       = IntPtr;
-using MossXR_HandModifier = IntPtr;
-using MossXR_BodyModifier = IntPtr;
-using MossXR_FaceModifier = IntPtr;
-using MossXR_Time = Int64;
-
-
 public enum MossXR_SessionState : byte{
             Unknown,
             Ready,
