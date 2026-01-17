b97087125e89ec848db0409e75989810c70fa79b
Toby <126118957+TxbiG@users.noreply.github.com>
2026-01-17 16:26:41 +0000

    Create MossXRNative.cs

---
+++ Native/XR/MossXRNative.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Runtime.InteropServices;
+
+
+namespace MossSharp.Native.XR
+{
+    using MossXR_Context      = IntPtr;
+    using MossXR_Session      = IntPtr;
+    using MossXR_Swapchain    = IntPtr;
+    using MossXR_Space        = IntPtr;
+    using MossXR_Action       = IntPtr;
+    using MossXR_ActionSet    = IntPtr;
+    using MossXR_Layer        = IntPtr;
+    using MossXR_Anchor       = IntPtr;
+    using MossXR_Origin       = IntPtr;
+    using MossXR_HandModifier = IntPtr;
+    using MossXR_BodyModifier = IntPtr;
+    using MossXR_FaceModifier = IntPtr;
+    using MossXR_Time = Int64;
+  
+    internal static class MossXRNative
+    {
+
+
+
+      
+    }
+}
