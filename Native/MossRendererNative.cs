b359f84637d501f87bb7f4d4834ae6a9355a5d5c
Toby <126118957+TxbiG@users.noreply.github.com>
2026-01-18 16:39:51 +0000

    Update MossRendererNative.cs

--- Native/MossRendererNative.cs
+++ Native/MossRendererNative.cs
@@ -4,7 +4,7 @@ using System.Runtime.InteropServices;
 
 namespace MossSharp.Native.Renderer
 {
-    internal static class MossCoreNative
+    internal static class MossRendererNative
     {
     }
 }
